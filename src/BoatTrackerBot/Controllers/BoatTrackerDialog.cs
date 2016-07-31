b7d326fdf15ef85751e1ecc2e36d8b83844b25f2
Tony Andrews <tonyandrews@hotmail.com>
2016-07-31 11:50:23 -0700

    Added initial LUIS integration. For now, we just try to identify the kind of request being made.

---
+++ src/BoatTrackerBot/Controllers/BoatTrackerDialog.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BoatTracker.Bot
+{
+    [LuisModel("b85dc175-ebf1-450c-96a8-0f7ffe0fd1b7", "bccaeb74ae1648c7a7eb38debaa7de21")]
+    [Serializable]
+    public class BoatTrackerDialog : LuisDialog<object>
+    {
+        public const string EntityBoatName = "boatName";
+        public const string EntityStart = "DateTime::start";
+        public const string EntityDuration = "DateTime::duration";
+        public const string EntityBuiltinDateTime = "builtin.datetime.date";
+        public const string EntityBuiltinDuration = "builtin.datetime.duration";
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = "Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("CreateReservation")]
+        public async Task CreateReservation(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to create a boat reservation");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("CheckBoatAvailability")]
+        public async Task CheckBoatAvailability(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to check on the availability of a boat");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Checkout")]
+        public async Task Checkout(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to check out a boat");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Checkin")]
+        public async Task Checkin(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to check in a boat");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("CheckReservations")]
+        public async Task CheckReservations(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to check on your reservations");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("CancelReservation")]
+        public async Task CancelReservation(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("It sounds like you want to cancel a reservation");
+            context.Wait(MessageReceived);
+        }
+    }
+}
--- src/BoatTrackerBot/Controllers/MessagesController.cs
+++ src/BoatTrackerBot/Controllers/MessagesController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Linq;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace BoatTracker.Bot
 {
@@ -17,53 +18,31 @@ namespace BoatTracker.Bot
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
+        [ResponseType((typeof(void)))]
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity != null)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, () => new BoatTrackerDialog());
+                        break;
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
-        }
+                    case ActivityTypes.Ping:
+                        break;
 
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
             }
 
-            return null;
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
         }
     }
 }
