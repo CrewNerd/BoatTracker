6ba8ea5057689e59d9bae77b9d2e92596c9b6458
Tony Andrews <tonyandrews@hotmail.com>
2016-07-31 14:04:41 -0700

    Moved the MSA app key & password out of the web.config file for security reasons.

--- src/BoatTrackerBot/Controllers/BoatTrackerDialog.cs
+++ src/BoatTrackerBot/Controllers/BoatTrackerDialog.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Luis.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 
 namespace BoatTracker.Bot
 {
