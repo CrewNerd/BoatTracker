130119ac930cbb0647eac358f090104cd8d7fcd4
Tony Andrews <tonyandrews@hotmail.com>
2016-07-31 22:48:16 -0700

    Added a little more intelligence, mostly to the CreateReservation intent. We now try to find and parse the boat name, start date & time, and the duration. If all of those are present, we present it to the user and ask for confirmation.

--- src/BoatTrackerBot/Controllers/BoatTrackerDialog.cs
+++ src/BoatTrackerBot/Controllers/BoatTrackerDialog.cs
@@ -16,7 +16,8 @@ namespace BoatTracker.Bot
         public const string EntityBoatName = "boatName";
         public const string EntityStart = "DateTime::start";
         public const string EntityDuration = "DateTime::duration";
-        public const string EntityBuiltinDateTime = "builtin.datetime.date";
+        public const string EntityBuiltinDate = "builtin.datetime.date";
+        public const string EntityBuiltinTime = "builtin.datetime.time";
         public const string EntityBuiltinDuration = "builtin.datetime.duration";
 
         public BoatTrackerDialog(ILuisService service)
@@ -35,7 +36,57 @@ namespace BoatTracker.Bot
         [LuisIntent("CreateReservation")]
         public async Task CreateReservation(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("It sounds like you want to create a boat reservation");
+            var boatName = this.FindBoatName(result);
+            var startDate = this.FindStartDate(result);
+            var startTime = this.FindStartTime(result);
+            var duration = this.FindDuration(result);
+
+            string replyMessage = string.Empty;
+
+            if (boatName == null)
+            {
+                replyMessage = "I think you want to reserve a boat but I can't find a boat name in your request.";
+            }
+            else if (startDate == DateTime.MinValue)
+            {
+                replyMessage = "I think you want to reserve a boat but I don't know what day you want to reserve it for.";
+            }
+            else if (startTime == DateTime.MinValue)
+            {
+                replyMessage = "I think you want to reserve a boat but I don't know what time you want to reserve it for.";
+            }
+            else if (duration == TimeSpan.Zero)
+            {
+                replyMessage = "I think you want to reserve a boat but I don't know how long you want to use it.";
+            }
+            else
+            {
+                string confirmMsg = string.Format(
+                    "Do you want to reserve boat '{0}' on {1} at {2} for {3} minutes?",
+                    boatName,
+                    startDate.ToLongDateString(),
+                    startTime.ToShortTimeString(),
+                    duration.TotalMinutes);
+
+                PromptDialog.Confirm(context, AfterConfirming_CreateReservation, confirmMsg, promptStyle: PromptStyle.None);
+                return;
+            }
+
+            await context.PostAsync(replyMessage);
+            context.Wait(MessageReceived);
+        }
+
+        public async Task AfterConfirming_CreateReservation(IDialogContext context, IAwaitable<bool> confirmation)
+        {
+            if (await confirmation)
+            {
+                await context.PostAsync("Okay, your reservation is confirmed!");
+            }
+            else
+            {
+                await context.PostAsync("Okay, I'm aborting that reservation.");
+            }
+
             context.Wait(MessageReceived);
         }
 
@@ -43,35 +94,131 @@ namespace BoatTracker.Bot
         public async Task CheckBoatAvailability(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("It sounds like you want to check on the availability of a boat");
+            string boatName = this.FindBoatName(result);
+
+            if (string.IsNullOrEmpty(boatName))
+            {
+                await context.PostAsync("It sounds like you want to check on the availability of a boat but I don't know how to do that yet.");
+            }
+            else
+            {
+                await context.PostAsync($"It sounds like you want to check on the availability of the '{boatName}' but I don't know how to do that yet.");
+            }
+
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("Checkout")]
         public async Task Checkout(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("It sounds like you want to check out a boat");
+            string boatName = this.FindBoatName(result);
+
+            if (string.IsNullOrEmpty(boatName))
+            {
+                await context.PostAsync("It sounds like you want to check out a boat but I don't know how to do that yet.");
+            }
+            else
+            {
+                await context.PostAsync($"It sounds like you want to check out the '{boatName}' but I don't know how to do that yet.");
+            }
+
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("Checkin")]
         public async Task Checkin(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("It sounds like you want to check in a boat");
+            string boatName = this.FindBoatName(result);
+
+            if (string.IsNullOrEmpty(boatName))
+            {
+                await context.PostAsync("It sounds like you want to check in a boat but I don't know how to do that yet.");
+            }
+            else
+            {
+                await context.PostAsync($"It sounds like you want to check in the '{boatName}' but I don't know how to do that yet.");
+            }
+
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("CheckReservations")]
         public async Task CheckReservations(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("It sounds like you want to check on your reservations");
+            await context.PostAsync("It sounds like you want to check on your reservations but I don't know how to do that yet.");
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("CancelReservation")]
         public async Task CancelReservation(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("It sounds like you want to cancel a reservation");
+            await context.PostAsync("It sounds like you want to cancel a reservation but I don't know how to do that yet.");
             context.Wait(MessageReceived);
+        }
+
+        private string FindBoatName(LuisResult result)
+        {
+            EntityRecommendation boatName;
+
+            if (result.TryFindEntity(EntityBoatName, out boatName))
+            {
+                return boatName.Entity;
+            }
+
+            return null;
+        }
+
+        private DateTime FindStartDate(LuisResult result)
+        {
+            EntityRecommendation builtinDate = null;
+            result.TryFindEntity(EntityBuiltinDate, out builtinDate);
+
+            if (builtinDate != null && builtinDate.Resolution.ContainsKey("date"))
+            {
+                var parser = new Chronic.Parser();
+                var span = parser.Parse(builtinDate.Entity);
+
+                if (span != null)
+                {
+                    var when = span.Start ?? span.End;
+                    return when.Value;
+                }
+            }
+
+            return DateTime.MinValue;
+        }
+
+        private DateTime FindStartTime(LuisResult result)
+        {
+            EntityRecommendation builtinTime = null;
+            result.TryFindEntity(EntityBuiltinTime, out builtinTime);
+
+            if (builtinTime != null && builtinTime.Resolution.ContainsKey("time"))
+            {
+                var parser = new Chronic.Parser();
+                var span = parser.Parse(builtinTime.Entity);
+
+                if (span != null)
+                {
+                    var when = span.Start ?? span.End;
+                    return when.Value;
+                }
+            }
+
+            return DateTime.MinValue;
+        }
+
+        private TimeSpan FindDuration(LuisResult result)
+        {
+            EntityRecommendation builtinDuration = null;
+            result.TryFindEntity(EntityBuiltinDuration, out builtinDuration);
+
+            if (builtinDuration != null && builtinDuration.Resolution.ContainsKey("duration"))
+            {
+                return System.Xml.XmlConvert.ToTimeSpan(builtinDuration.Resolution["duration"]);
+            }
+
+            return TimeSpan.Zero;
         }
     }
 }
