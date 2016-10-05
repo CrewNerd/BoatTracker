2d428dcec6f2ff8c110b0074eb88edfb92388f20
Tony Andrews <tonyandrews@hotmail.com>
2016-10-04 21:04:14 -0700

    Exploratory work on adding a status web page for kiosk use.

---
+++ src/BoatTrackerBot/App_Code/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using BoatTracker.Bot.Configuration;
+
+static public class Helpers
+{
+    public static string ClubName(string clubId)
+    {
+        return EnvironmentDefinition.Instance.MapClubIdToClubInfo[clubId].Name;
+    }
+}
