9800e5626992de88f16b7c629d57cab8b774c513
Tony Andrews <tonyandrews@hotmail.com>
2016-10-06 22:47:31 -0700

    First commit of the nightly WebJob. It doesn't do anything yet.

---
+++ src/BoatTrackerWebJob/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace BoatTrackerWebJob
+{
+    public class Functions
+    {
+        [NoAutomaticTrigger]
+        public static void RunPolicyChecks([Blob("container/policychecklog.txt")] TextWriter log)
+        {
+            log.WriteLine($"Policy checks starting at {DateTime.UtcNow.ToString()}");
+
+            // TODO: implement policy checks
+            // TODO: can we arrange to run the checks at 1am *local time* for each club?
+
+            log.WriteLine($"Policy checks complete at {DateTime.UtcNow.ToString()}");
+        }
+    }
+}
---
+++ src/BoatTrackerWebJob/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+
+namespace BoatTrackerWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var host = new JobHost();
+            host.Call(typeof(Functions).GetMethod("RunPolicyChecks"));
+        }
+    }
+}
