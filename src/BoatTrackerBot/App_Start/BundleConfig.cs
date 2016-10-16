1e50ec32c0478fa74d00d22a0d5cfe050ff8d72d
Tony Andrews <tonyandrews@hotmail.com>
2016-10-16 15:55:21 -0700

    Configuration fixes - we need jQuery after all.

--- src/BoatTrackerBot/App_Start/BundleConfig.cs
+++ src/BoatTrackerBot/App_Start/BundleConfig.cs
@@ -8,13 +8,11 @@ namespace BoatTracker.Bot
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-#if false
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-#endif
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
