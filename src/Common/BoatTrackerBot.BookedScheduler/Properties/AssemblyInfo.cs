4d19b89aabcefecbd1effa2282a384982de085dd
Tony Andrews <tonyandrews@hotmail.com>
2016-10-30 16:06:20 -0700

    Moved the BS client to a non-portable class library so we can refactor things better.

--- src/Common/BoatTrackerBot.BookedScheduler/Properties/AssemblyInfo.cs
+++ src/Common/BoatTrackerBot.BookedScheduler/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BookedScheduler")]
+[assembly: AssemblyTitle("BoatTrackerBot.BookedScheduler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BookedScheduler")]
+[assembly: AssemblyProduct("BoatTrackerBot.BookedScheduler")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46367a47-45df-4458-8af0-d57434a620eb")]
 
 // Version information for an assembly consists of the following four values:
 //
